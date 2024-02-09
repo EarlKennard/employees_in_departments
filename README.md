a8d6fcd74dd70f27383c53f1fb7e9f90e44e0d7f
EarlKennard <githubearl@gmail.com>
2024-02-08 18:33:46 -0700

    changed the README to reflect the current state of the project. finally 'finished' the project so that it was in a 'working' state.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Project description
 
-Once finished, this program will be a text interface where you can create a company, add some departments to it, and add some workers to the departments. You'll also be able to delete companies/departments/workers.
+This program is a text interface where you can add employees to work in departments and fire existing ones. You can also see an alphabetical list of workers in the entire
+company or just in certain departments.
 
 # How to run it
 
@@ -8,15 +9,14 @@ Clone the repo and make sure you have Rust installed in your VSCODE. Just type i
 
 # Problems
 
-1. Not finished yet
+1. There is a list of current major problems at the end of main.rs, if you are curious
 
 # Future fixes/planned or possible changes
 
 1. fixing everything in the problems section
-2. In the future, I may add the ability to separate workers in a department into different types, like managers/supervisors/normal workers; add the ability to designate someone as the head of a department; and add the ability to designate someone the CEO, CTO, etc. 
-3. As always, the possibility of creating a UI that's better than a text interface in the future exists.
-4. There are other functionalities/properties to a company I could add, like indicating whether or not it's in the red, but I'm not sure about the necessity of such features.
+2. As of now, aside from fixing all of the problems, I may add the ability to create more than one company. After that, there may be other minor modifications/updates.
+3. Once it makes sense to do so, I'll separate the source code into different files.
 
 # Inspiration for the project
 
-This is an expanded modification of the suggested hashmap exercise in the Rust book: https://doc.rust-lang.org/stable/book/ch08-03-hash-maps.html. 
+This is an slightly expanded modification of the suggested hashmap exercise in the Rust book: https://doc.rust-lang.org/stable/book/ch08-03-hash-maps.html. 
--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,150 @@ use std::io;
 use std::collections::HashMap;
 
 fn main() {
-    println!("Hello, world! This is a program that adds workers to departments in a company. You can create several companies with several departments and 
-    several workers.");
+    println!("Hello, world! This is a text interface program where you can add employees to/delete from a department in a company.");
+    list_of_commands();
 
-    let mut company_vector: Vec<company> = Vec::new();
+    let mut company: HashMap<String, Vec<String>> = HashMap::new();
+
+    loop {
+        let input = user_input_str();
+        let inputs: Vec<&str> = input.split_whitespace().collect(); // idk why i have to split these up to input and inputs
+
+        match inputs[0] {
+            "quit" => break,
+            "commands" => list_of_commands(),
+            "add" => add_worker(&mut company, inputs[3], inputs[1]),
+            "delete" => fire_worker_interface(&mut company, inputs[3], inputs[1]),
+            "workers" => {
+                if inputs[2] == "company" {
+                    sort_workers_in_company(&company);
+                } else {
+                    sort_workers_in_department(&company, inputs[2]);
+                }
+            },
+            "departments" => sort_departments(&company),
+            _ => println!("Wrong command. Please try again."),
+        }
+    }
 }
 
-struct company {
-    name: String,
-    departments_and_workers: HashMap<String, Vec<String>>,
-}
-
-// DRY
-fn user_input_string() -> String {
+fn user_input_str() -> String {
     let mut input = String::new();
 
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line!");
 
+    input = input.trim().to_lowercase().to_string();
+
     input
 }
 
 fn list_of_commands() {
-    println!("\nHere are the commands:
-    create_company: creates a new company. you have to name the company and at least create one department with one worker in it.
-    delete_company: delete a company
-    create_department: given an existing company, creates a new department and you have to put at least one worker in it.
-    delete_department: given an existing company, you can delete an entire department
-    hire_worker: given the name of a company and a department in it, you can hire a worker
-    fire_worker: given the name of a company and a department in it, you can fire a worker
-    companies: show the list of existing companies, sorted alphabetically
-    company_details: given an existing company, show the departments listed alphabetically
-    department_details: given an existing company and department, show the workers in it sorted alphabetically\n");
+    println!("For now, you can only add, delete, or retrieve a list of people in a department/company. Examples of available commands:
+    Add <Worker> to <Department> -> adds a worker to a department. creates a new department if one doesn't already exist
+        Example: Add Sally to Engineering
+    Delete <Worker> from <Department> -> deletes worker from a department. the worker and the department both have to exist.
+        Example: Delete Sally from Engineering
+    Workers in <Department> -> alphabetically lists workers from a department, like Engineering. Replace <Department> with the appropriate department.
+        Example: Workers in Engineering
+    Workers in <Company> -> alphabetically lists workers in the company. For now there is only one company, so only the following command works:
+        Example: Workers in company
+    Departments in <Company> -> alphabetically lists the departments in a company. For now, there is only one company, so only the following command works:
+        Example: Departments in company
+    
+    To see this list of commands again, type 'commands'.
+    Type 'quit' to end the program.
+    Please note that worker and department names should only be a single word each.");
+}
+
+fn sort_workers_in_department(company: &HashMap<String, Vec<String>>, user_department: &str) {
+    if !company.contains_key(user_department) {
+        println!("Sorry! That department doesn't exist!");
+        return;
+    }
+
+    let mut names: Vec<String> = Vec::new();
+    for (department, employees) in company {
+        if department.to_string() == user_department.to_string() {
+            for employee in employees {
+                names.push(employee.to_string());
+            }
+        }
+    }
+
+    names.sort_by(|a, b| a.cmp(&b));
+    println!("Here are the workers in {}: {:?}", user_department, names);
+}
+
+fn sort_workers_in_company(company: &HashMap<String, Vec<String>>) {
+    let mut names: Vec<String> = Vec::new();
+    for (_department, employees) in company {
+        for employee in employees {
+            names.push(employee.to_string());
+        }
+    }
+
+    names.sort_by(|a, b| a.cmp(&b));
+    println!("Here are the workers in the company: {:?}", names);
+}
+
+fn sort_departments(company: &HashMap<String, Vec<String>>) {
+    let mut departments: Vec<String> = Vec::new();
+    for (department, _employees) in company {
+        departments.push(department.to_string());
+    }
+
+    departments.sort_by(|a, b| a.cmp(&b));
+    println!("Here are the departments in the company: {:?}", departments);
+}
+
+fn add_worker(company: &mut HashMap<String, Vec<String>>, department: &str, worker: &str) {
+    company.entry(department.to_string()).or_insert(Vec::new());
+    company.get_mut(department).unwrap().push(worker.to_string());
+
+    println!("You just hired {} to work in {}!", worker, department);
+}
+
+fn fire_worker(company: &mut HashMap<String, Vec<String>>, department: &str, worker: &str) -> bool {
+    if !company.contains_key(department) {
+        return false;
+    }
+
+    // unwrap explicitly returns Some, but will actually panic if there is None, which is why it's discouraged. 
+    // Good thing the previous if statement quits the fn if there actually is None
+    if !company.get(department).unwrap().contains(&worker.to_string()) {
+        return false;
+    }
+
+    // removes every instance
+    company.get_mut(department).unwrap().retain(|x| *x != worker.to_string());
+
+    true
+}
+
+// needed cuz the match statement in main only accepts () due to the other arms
+// con (i think): linked dependency from fire_worker() to this. not that big of a con imo
+fn fire_worker_interface(company: &mut HashMap<String, Vec<String>>, department: &str, worker: &str) {
+    let checker = fire_worker(company, department, worker);
+
+    if checker == false {
+        println!("Sorry! {} doesn't work in {}!", worker, department);
+    } else {
+        println!("{} has been fired from {}.", worker, department);
+    }
 }
 
 /* Notes
-1. should i initialize some departments in the hashmap?
-2. there are no types of workers because that seems too annoying for now
+1. nah let's just make this simple and expand this if need be later because i'm really confused
+2. to make things simpler, names are added in lowercase so i don't have to check for cases. this isn't ideal, but i'll add that functionality later
 
-3. commands that the user should be able to do
-a) create_company: creates a new company
-b) delete_company: deletes a company (idk how to do this)
-c) create_department: has to give the name of the company. create a department in that
-d) delete_department: has to give the name of company. delete department
-e) hire_worker: has to give name of department and company. push worker in vector
-f) fire_worker: has to give name of department and company. pop worker from vector
-g) companies: show companies sorted alphabetically. not too much information cuz the terminal would bloat, just show names
-h) company_details: show details of a company. only name, and the departments sorted alphabetically
-i) department_details: given a company, show the workers in department sorted alphabetically
+list of problems:
+3. can only do very simple commands
+a) thread main panics if the command starts with a valid word (aka doesn't satisfy the else case in the match statement) and the arm reaches for an index position that
+is non-existent because the inputs vector in main isn't long enough (aka a command like 'workers inoaisdufoaisjdf' and "workers" arm reaches for inputs[2]).
+index out of bounds basically, but i had to describe how it happens
+b) names of workers and departments can only be one single word each, or else the program starts breaking down and gets confusing. it's a very big limitation. 
+to fix this, i would have to really expand the user input interface
+4. currently there is no way to just create a department with no workers. seems like a major and dumb oversight
 */
